382f56170824971d76c5bfa117f77853d338ffab
Angelo Vreeswijk <angelo.vreeswijk@wetransfer.com>
2020-12-23 17:05:39 +0100

    Added json from data

--- js/main.js
+++ js/main.js
@@ -13,6 +13,21 @@ tippy('.winning-score', {
 
 const amountWins = document.querySelectorAll('[data-wins]');
 
+import * as data from '../data.json';
+const word = data[0].tableContent;
+word.splice(0, 37); 
+word.splice(-2,2)
+var regex = /(<([^>]+)>)/ig;
+for(var i=0;i<word.length;i++){
+  word[i] = word[i].replace(regex, "");
+  }
+
+var i,j,temparray,chunk = 39;
+for (i=0,j=word.length; i<j; i+=chunk) {
+    temparray = word.slice(i,i+chunk);
+    console.log(temparray);
+}
+
 amountWins.forEach(function(el) {
     let amount = el.dataset.wins;
     let amountPlayed = 13;
