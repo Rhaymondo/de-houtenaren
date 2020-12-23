d2fc71df559e049b1309be012198d8b323ef8d6f
Angelo Vreeswijk <angelo.vreeswijk@wetransfer.com>
2020-12-23 10:15:35 +0100

    First setup

---
+++ js/main.js
@@ -0,0 +1,41 @@
+tippy('.winning-score', {
+    content: 'Gefeliciteerd goed gedaan!! 🎉',
+    animation: 'scale',
+    theme: 'light'
+  });
+
+  tippy('.go-team', {
+    content: 'Klik om team te bekijken ',
+    animation: 'scale',
+    placement: 'left-end',
+    theme: 'light'
+  });
+
+const amountWins = document.querySelectorAll('[data-wins]');
+
+amountWins.forEach(function(el) {
+    let amount = el.dataset.wins;
+    let amountPlayed = 10;
+    let amountPerRound = 10;
+    let amountPerRoundWin = amountPerRound * 5;
+    const balance = (amountPerRound * amountPlayed) - (amount * amountPerRoundWin)
+    if(balance > 0) {
+        el.parentNode.querySelector('[data-balance]').innerText = "-" + balance + " €";
+        el.parentNode.querySelector('[data-balance]').classList.add('is-negative');
+        tippy('.is-negative', {
+            content: 'Betalen 🤦',
+            animation: 'scale',
+            theme: 'light',
+            placement: 'right-end',
+          });
+    } else {
+        el.parentNode.querySelector('[data-balance]').innerText = balance.toString().replace("-", "+ ")  + " €";
+        el.parentNode.querySelector('[data-balance]').classList.add('is-positive');
+        tippy('.is-positive', {
+            content: 'Money Money Money 💰',
+            animation: 'scale',
+            theme: 'light',
+            placement: 'right-end',
+          });
+    }
+});
